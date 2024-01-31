c530daa5974dc2254aa9e19a1ce68060e74931a4
Nathália Beatriz <limayx3@users.noreply.github.com>
2024-01-30 21:30:16 -0300

    30.01
    Criação dos arquivos dos exercícios de ex066 à ex071 e execução do ex066.

---
+++ ex066.py
@@ -0,0 +1,13 @@
+#LER VÁRIOS NÚMEROS INTEIROS, PARANDO QUANDO O 999 (FLAG) FOR DIGITADO.
+#MOSTRAR QUANTOS NÚMEROS FORAM DIGITADOS E SUA SOMA DESCONSIDERANDO O FLAG.
+
+c = n = s = 0
+
+while True:
+    n = int(input('Digite um número inteiro (999 para parar): '))
+    if n == 999:
+        break
+    c += 1
+    s += n
+
+print(f'Você digitou {c} números e o somatório deles é {s}.')
