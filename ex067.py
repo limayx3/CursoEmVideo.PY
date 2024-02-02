88d3c252d32cedd88509fae2887bb8a4bf83549f
Nathália Beatriz <limayx3@users.noreply.github.com>
2024-02-01 21:08:08 -0300

    2024.02.01
    Execução dos exercícios ex067 e ex068.

--- ex067.py
+++ ex067.py
@@ -1,2 +1,18 @@
 #MOSTRAR TABUADA DO NÚMERO QUE A PESSOA ESCOLHER, PARA CADA VALOR DIGITADO PELO USER.
 #PROGRAMA É INTERROMPIDO QUANDO NÚMERO DIGITADO FOR NEGATIVO.
+
+n = 0
+c = 1
+
+while True:
+    n = int(input('\nDigite o número que quer saber a tabuada (valor negativo para parar): '))
+    if n < 0:
+        break
+    print('=' * 25)
+    print(f'Tabuada de {n}:')
+    print('='* 25)
+    for c in range (1, 11):
+        print(f'{n:>6} x {c:^2} = {n*c}')
+        c += 1
+    print('='* 25)
+print('PrOgRaMa FiNaLiZaDo.\n')
--- ex068.py
+++ ex068.py
@@ -1,2 +1,35 @@
 #JOGAR PAR OU ÍMPAR COM O PC, INTERROMPENDO QUANDO O USER PERDER.
 #AO FINDAR, MOSTRAR TOTAL DE VITÓRIAS DO USER.
+
+import random
+import time
+
+player1 = player2 = count = soma = 0
+
+while True:
+    print('=' * 100)
+    player1 = int(input('Escolha um número inteiro de 0 à 10: '))
+    player2 = random.randint(0,10)
+    soma = player1 + player2
+    escolha = str(input('"p" para par ou "i" para ímpar? ')).strip().lower()[0]
+    for c in range (1, 4):
+        time.sleep(0.5)
+        print(f'{c}...')
+
+
+    if escolha == 'p':
+        if soma % 2 != 0:
+            break
+        else:
+            print(f'Deu PAR!\n')
+            count += 1        
+    elif escolha == 'i':
+        if soma % 2 == 0:
+            break
+        else:
+            print(f'Deu ÍMPAR!\n')
+            count += 1
+            
+print('=' * 100)
+print(f'Xiiiiii....... Deu ruim após {count} vitórias, meu consagrado.')
+print('=' * 100)
