14f869d28a19b27172a50b8369b597c59c0d22f2
Nathália Beatriz <limayx3@users.noreply.github.com>
2024-02-11 18:56:59 -0300

    2024.02.11
    Tentativa falha de fazer exercícios ex073 e ex074... ta ficando difícil

--- ex073.py
+++ ex073.py
@@ -2,4 +2,12 @@
 #A - APENAS OS 5 PRIMEIROS;
 #B - OS ÚLTIMOS 4 COLOCADOS;
 #C - TODOS EM ORDEM ALFABÉTICA; E
-#D - POSIÇÃO DO CHAPECOENSE;
+#D - POSIÇÃO DO CHAPECOENSE/ FLAMENGO;
+
+brasileirão = ('Palmeiras', 'Grêmio', 'Atlético-MG', 'Flamengo', 'Botafogo', 'Bragantino', 'Fluminense', 'Athletico-PR', 'Internacional', 'Fortaleza', 'São Paulo', 'Cuiabá', 'Corinthians', 'Cruzeiro', 'Vasco', 'Bahia', 'Santos', 'Goiás', 'Coritiba', 'América-MG')
+count = 1
+
+print(f'Os 5 primeiros são, em ordem de colocação: \n→{brasileirão[0:5]}')
+print(f'A zona de rebeixamento é formada por: \n→{brasileirão[20:15:-1]}')
+print(f'Em ordem alfabética: \n→{sorted(brasileirão)}')
+
--- ex074.py
+++ ex074.py
@@ -1,1 +1,6 @@
 #GERAR 5 NÚMEROS ALEATÓRIOS DE 0 À 10, EXIBIR TAL LISTA E INFORMAR QUAL MAIOR E QUAL MENOR VALORES.
+
+from random import randint
+
+numeros = randint(0, 10)
+print(numeros)
