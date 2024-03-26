9dddd62dc167bd32f2dd0a13b61a4f2959509ff5
Nathália Beatriz <limayx3@users.noreply.github.com>
2024-03-25 21:40:23 -0300

    2024.03.25
    Tentativa de executar ex095.

--- ex095.py
+++ ex095.py
@@ -1,1 +1,30 @@
+#Gerenciador de aproveitamento de jogador que lê o nome, quantas partidas jogadas e quantos gols feitos por partida.
+#Tudo guardado num dicionário, incluindo o total de gols feitos no campeonato
 #Aprimorar ex093, para funcionar com vários jogadores, incluindo exibir lista com códigos pra cada um e visualização de detalhe individual por código.
+
+jogadores = []
+jogador = {}
+gols = []
+média = soma = gol = 0
+
+while True:
+    jogador['nome'] = str(input('Nome do jogador: '))
+    jogador['partidas'] = int(input('Partidas jogadas: '))
+    for p in range(0, jogador['partidas']):
+        gol = int(input(f'Total de gols da {p+1}ª partida: '))
+        soma += gol
+        gols.append(gol)
+    jogador['gols'] = gols[:]
+    jogador['totgols'] = soma
+    média = soma / jogador['partidas']
+    jogadores = jogador.copy()
+    jogador.clear()
+
+    continuar = str(input('Deseja continuar? [S/N] ')).strip().upper()
+    if continuar == 'N':
+        break
+print(f'{"Cód":<5}|{"Nome do Jogador":^30}')
+for j in range(0, len(jogadores)):
+    print(f'{j:<5}|{jogadores[j]['nome']:^30}')
+
+
